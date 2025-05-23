129e21b19babc9b5d981d4193997330bf30e8c5c
an-12195 <165748978+an-12195@users.noreply.github.com>
2025-05-23 20:25:52 +0530

    trying

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,5 +1,6 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../firebase-service-account.json"); // Rename your downloaded Firebase admin key to this
+
+const serviceAccount = require("/etc/secrets/serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),

e64b92702e192d01d3eb831d23ea1962d82c3115
an-12195 <165748978+an-12195@users.noreply.github.com>
2025-05-23 21:07:47 +0530

    fdfvdgdf

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use("/api", protectedRoutes);
 // Serve static files (Vite's build)
 app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-app.get("/(.*)/", (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 

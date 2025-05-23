b836b35d40591e9e301fc958268c09bd09ce85bc
an-12195 <165748978+an-12195@users.noreply.github.com>
2025-05-23 15:22:54 +0530

    fdfs

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,6 +1,12 @@
 const admin = require('firebase-admin');
+const fs = require('fs');
+const path = require('path');
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// Path to your secret file
+const serviceAccountPath = '/etc/secrets/firebaseServiceAccount.json';
+
+// Read and parse the service account JSON
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
